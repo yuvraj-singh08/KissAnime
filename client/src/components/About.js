d737cdc05cf812a774f2c674f3ebc11101990c78
Priyanshu <20bec076@iiitdmj.ac.in>
2023-06-19 13:54:34 +0530

    card and about section changed

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -3,14 +3,14 @@ import './css/About.css';
 const About = () => {
     return (
       <div className="py-14">
-        
+{/*         
         <div className=" flex justify-center my-8">
           <h1 className="text-5xl font-extrabold text-center text-white">
             ABOUT US
           </h1>
         </div>
         <div className="flex">
-          <p className="text-white mx-20 p-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab a vero cupiditate quod illum? Ullam neque ex animi molestias blanditiis nesciunt soluta assumenda unde veniam consequatur itaque, fuga rem atque voluptas repellendus! Officia sint iusto, architecto quo sapiente quaerat aliquid vero distinctio ex, voluptates natus cum vel commodi, ducimus assumenda! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia sapiente totam ullam expedita architecto accusantium nisi exercitationem consequatur a alias voluptatem eius in quia, sit ab mollitia, culpa harum qui?</p>
+          <p className="text-white mx-20 p-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab a vero cupiditate quod illum? Ullam neque ex animi molestias blanditiis nesciunt soluta assumenda unde veniam consequatur itaque, fuga rem atque voluptas repellendus!</p>
           
      <div className="text-white mr-24">
       <h1 className="text-white py-2 text-center text-3xl">THE TEAM</h1>
@@ -36,12 +36,12 @@ const About = () => {
             </div>
   
             <div className="wrapper">
-              <a href="#demo-modal">Harshil Sharma</a>
+              <a href="#demo-modal">Yuvraj Singh</a>
             </div>
   
             <div id="demo-modal" className="modal">
               <div className="modal__content">
-                <h1>HARSHIL SHARMA</h1>
+                <h1>Yuvraj Singh</h1>
   
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde tempore enim, minima magni placeat nihil magnam qui tempora ullam doloribus mollitia aliquam, numquam repellat eum ipsum nobis nostrum temporibus saepe amet eveniet, molestias earum facere. Laboriosam laborum reprehenderit odio inventore.
@@ -56,7 +56,25 @@ const About = () => {
             </div>
           </div>
           </div>
-          </div>
+          </div> */}
+          <div class="flex">
+   
+
+  </div>
+  <h1 class=" text-center mx-auto w-96   mb-12 font-mono font-extrabold text-3xl text-white hover:underline cursor-pointer p-4 font-extrabold font-mono" id="about">ABOUT US</h1>
+  <div class="flex">
+    <div>
+   <p class=" text-white  font-sans font-thin mx-16"> <strong> Lorem, ipsum dolor sit </strong>amet consectetur adipisicing elit. Praesentium culpa numquam aspernatur. Possimus odio, neque iusto voluptatem ut deleniti! Aperiam, laudantium. Perspiciatis fugiat molestiae possimus, nisi laboriosam quidem laudantium consequuntur, itaque ducimus ullam accusamus nobis accusantium repellat molestias eligendi tenetur est asperiores rem. Ex, reprehenderit!  </p>
+
+   <p class=" text-white font-sans font-thin mx-16 mt-6" > <strong> Lorem ipsum dolor, sit </strong> amet consectetur adipisicing elit. Dicta impedit dolorem doloremque placeat ut nesciunt totam quae necessitatibus harum asperiores! Nostrum molestias rerum quaerat distinctio eos doloribus architecto eum autem in reiciendis, eveniet sequi provident iste perspiciatis totam cum cupiditate deserunt illum iure. Dolorem ducimus neque, voluptates earum magni illum!</p>
+
+   <p class=" text-white  font-sans font-thin mx-16 mt-6" > <strong> Lorem ipsum dolor, sit  </strong>amet consectetur adipisicing elit. Dicta impedit dolorem doloremque placeat ut nesciunt totam quae necessitatibus harum asperiores! Nostrum molestias rerum quaerat distinctio eos doloribus architecto eum autem in reiciendis, eveniet sequi provident iste perspiciatis totam cum cupiditate deserunt illum iure. Dolorem ducimus neque, voluptates earum magni illum! </p>
+  </div>
+
+   <img src="https://www.pngmart.com/files/6/Attack-On-Titan-PNG-Photo-1.png"  class="h-96 w-full mr-16  hover:border-amber-600  " alt="no upload"/>
+
+  </div>
+<div class="bg-white"></div>
           </div>
         
       
